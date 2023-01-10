962867791f43d24deef8297f70afc1e6c3099860
flo <f.imbert4@gmail.com>
2023-01-10 11:37:49 +0100

    replace axios with import json from ./assets/data/...json

--- src/components/Accommodation.jsx
+++ src/components/Accommodation.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 import Card from "./Card";
+import data from "../assets/data/logements.json";
 
 // Get accommodation data from API
 // Create <UL> with Card component and provide data from API as props to it
 const Accommodation = () => {
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
 
-    useEffect(() => {
-        axios.get("/logements/logements.json").then((res) => setData(res.data));
-    }, []);
+    // useEffect(() => {
+    //     axios.get("/logements/logements.json").then((res) => setData(res.data));
+    // }, []);
 
     return (
         <ul className="cardsContainer">
